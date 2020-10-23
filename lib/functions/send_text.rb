6ceaefc928721ed712ff7e3a5066b406a9223336
Алексей Ю. Алёшин <ay.aleshin@jet.su>
2020-10-23 14:41:07 +0300

    Доработан send_text (умеет слать return)

--- lib/functions/send_text.rb
+++ lib/functions/send_text.rb
@@ -5,7 +5,7 @@ module Functions
 
     VALIDATE_ERROR_MESSAGE = 'Должен быть заполнен один из атрибутов :value, :value_from_storage'.freeze
 
-    attr_accessor :value, :value_from_storage
+    attr_accessor :value, :value_from_storage, :send_return
     validates :selector, presence: true
 
     validates_each :value, :value_from_storage do |record, attr|
@@ -15,6 +15,9 @@ module Functions
     def done!(element)
       value = self.value_from_storage.present? ? storage[self.value_from_storage] : self.value
       element.send_keys(value)
+      if send_return.present?
+        element.send_keys(:return)
+      end
     end
   end
 end
